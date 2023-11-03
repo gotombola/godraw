f3ccb59fbf6af5b23b607813e19f4550d3adb799
astra <matthis.brocheton@epitech.eu>
2023-11-03 11:44:48 +0100

    feat: IgnoreTickets, PartialDraw, PartialMaxWinner

--- create_test.go
+++ create_test.go
@@ -125,8 +125,42 @@ func Test_3T_2B_1Q_10Q_2W(t *testing.T) {
 	testNum := 2
 	test := Data{
 		Tickets: []Ticket{{Data: "a", Id: "1", Owner: ""}, {Data: "a", Id: "1", Owner: "test"}, {Data: "a", Id: "1", Owner: "test"}},
-		Bunches: []Bunch{{Id: "toy1", Nb: 1}, {Id: "toy2", Nb: 10}},
+		Bunches: []Bunch{{Id: "toy1", Nb: 10}, {Id: "toy2", Nb: 10}},
 		Mode:    "lottery",
+	}
+	d, _ := CreateDraw(test)
+
+	if len(d.Winners) != testNum {
+		t.Errorf("Expected %d, got %d", testNum, len(d.Winners))
+	}
+}
+
+func Test_3T_3B_1Q_1Q_2W_1IT_0PM(t *testing.T) {
+	testNum := 2
+	test := Data{
+		Tickets:          []Ticket{{Data: "a", Id: "1", Owner: ""}, {Data: "a", Id: "2", Owner: ""}, {Data: "a", Id: "3", Owner: ""}},
+		Bunches:          []Bunch{{Id: "toy1", Nb: 1}, {Id: "toy2", Nb: 1}, {Id: "toy3", Nb: 1}},
+		Mode:             "lottery",
+		IgnoreTickets:    []Ticket{{Data: "a", Id: "2", Owner: ""}},
+		PartialDraw:      false,
+		PartialMaxWinner: 0,
+	}
+	d, _ := CreateDraw(test)
+
+	if len(d.Winners) != testNum {
+		t.Errorf("Expected %d, got %d", testNum, len(d.Winners))
+	}
+}
+
+func Test_3T_3B_1Q_10Q_2W_0IT_2PM(t *testing.T) {
+	testNum := 2
+	test := Data{
+		Tickets:          []Ticket{{Data: "a", Id: "1", Owner: ""}, {Data: "a", Id: "2", Owner: ""}, {Data: "a", Id: "3", Owner: ""}},
+		Bunches:          []Bunch{{Id: "toy1", Nb: 10}, {Id: "toy2", Nb: 10}, {Id: "toy3", Nb: 10}},
+		Mode:             "lottery",
+		IgnoreTickets:    []Ticket{},
+		PartialDraw:      true,
+		PartialMaxWinner: 2,
 	}
 	d, _ := CreateDraw(test)
 
