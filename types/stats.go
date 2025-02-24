b3298ffe42d7e25b241f6b5b88d8e24dad2d006a
emilienbuguet <72021370+emilienbuguet@users.noreply.github.com>
2025-02-24 17:53:45 +0100

    feat: stats on participants (#15)

--- types/stats.go
+++ types/stats.go
@@ -1,11 +1,16 @@
 package types
 
 type Stats struct {
-	NbTickets      int `json:"nb_tickets"`
-	NbDraws        int `json:"nb_draws"`
-	StartTimestamp int `json:"start_timestamp"`
-	EndTimestamp   int `json:"end_timestamp"`
-	Duration       int `json:"duration"`
+	NbTickets             int                   `json:"nb_tickets"`
+	NbDraws               int                   `json:"nb_draws"`
+	NbParticipants        int                   `json:"nb_participants"`
+	PercentWinnersAtLeast []WinnersPercentStats `json:"percent_winners_at_least"`
+	NbWinnersAtLeast      []WinnersStats        `json:"nb_winners_at_least"`
+	PercentWinnersExactly []WinnersPercentStats `json:"percent_winners_exactly"`
+	NbWinnersExactly      []WinnersStats        `json:"nb_winners_exactly"`
+	StartTimestamp        int                   `json:"start_timestamp"`
+	EndTimestamp          int                   `json:"end_timestamp"`
+	Duration              int                   `json:"duration"`
 }
 
 type StepStats struct {
@@ -18,4 +23,14 @@ type StepStats struct {
 	StartTimestamp     int    `json:"sts"`
 	EndTimestamp       int    `json:"ets"`
 	Duration           int    `json:"d"`
+}
+
+type WinnersStats struct {
+	NbBunches int `json:"nbb"`
+	Value     int `json:"v"`
+}
+
+type WinnersPercentStats struct {
+	NbBunches int     `json:"nbb"`
+	Value     float64 `json:"v"`
 }
